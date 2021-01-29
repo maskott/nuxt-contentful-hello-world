02ab7bf7c90fb0f950667146d94fb9df9b10cdc8
Mike Skott <mike@skott.us>
2021-01-29 01:07:50 -0500

    fixin thangz

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -2,9 +2,9 @@
     <div>
         <b-container class="post">
             <h2>
-                {{ post.title }}
+                {{ post.title[lang] }}
             </h2>
-            <rtr :document="post.body" />
+            <rtr :document="post.body[lang]" />
             <hr />
             <LangSwitcher />
         </b-container>
@@ -17,26 +17,41 @@ import client from '~/plugins/contentful.js'
 import richTextRenderer from 'contentful-rich-text-vue-renderer';
 
 export default {
+    nuxtI18n: {
+        paths: {
+            en: '/posts/:slug',
+            es: '/publicaciones/:slug'
+        }
+    },
     components: {
         rtr: richTextRenderer,
     },
-    asyncData({app, env, params}) {
+    asyncData({app, env, store, params}) {
         return Promise.all([
             client.getEntries({
                 'content_type': 'post',
-                'fields.slug': params.slug,
-                'locale': app.i18n.locale
+                ['fields.slug.' + app.i18n.locale]: params.slug,
+                'locale': "*"
             }),
             client.getEntries({
                 'content_type': 'post',
                 'locale': app.i18n.locale
             }),
         ]).then(([post, posts]) => {
+            store.dispatch('i18n/setRouteParams', {
+                en: { slug: post.items[0].fields.slug.en },
+                es: { slug: post.items[0].fields.slug.es }
+            })
             return {
                 post: post.items[0].fields,
                 posts: posts.items,
             }
         }).catch(console.error)
+    },
+    computed: {
+        lang () {
+            return this.$i18n.locale
+        }
     },
 }
 </script>
